67861d1fa807831db1bd1893663a6d0a356761a4
Greg Althaus <galthaus@austin.rr.com>
2016-01-11 21:09:46 -0600

    Fix pathmunge helper

--- script/roles/docker-swarm-install/20-set-gopath.sh
+++ script/roles/docker-swarm-install/20-set-gopath.sh
@@ -3,11 +3,11 @@
 cat >/etc/profile.d/gopath.sh <<EOF
 
 pathmunge () {
-        if ! echo $PATH | /bin/egrep -q "(^|:)$1($|:)" ; then
-           if [ "$2" = "after" ] ; then
-              PATH=$PATH:$1
+        if ! echo \$PATH | /bin/egrep -q "(^|:)\$1($|:)" ; then
+           if [[ \$2 = after ]] ; then
+              PATH=\$PATH:\$1
            else
-              PATH=$1:$PATH
+              PATH=\$1:\$PATH
            fi
         fi
 }
