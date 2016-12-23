cd02c16ac167541579de0cfef215475caba8e9cf
Rob @zehicle Hirschfeld <rob@rackn.com>
2016-12-23 11:48:01 -0600

    update version to v1.5.1, change update sums script because TAR layout changed

--- kompos8/update_k8s_sums.sh
+++ kompos8/update_k8s_sums.sh
@@ -13,19 +13,15 @@ mkdir /tmp/$$.dl
 CODE_DIR=`pwd`
 cd /tmp/$$.dl
 
-if [[ $FILE == "" ]] ; then
-  echo "Downloading $K8S_VERSION"
-  curl https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/kubernetes.tar.gz -o kubernetes.tgz
-else
-  echo "Copying $FILE"
-  cp $2 kubernetes.tgz
-fi
+echo "Downloading six components for $K8S_VERSION (SLOW way, no compression)"
+curl https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kube-apiserver -o kube-apiserver
+curl https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kkube-controller-manager -o kube-controller-manager
+curl https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kube-scheduler -o kube-scheduler
+curl https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kubelet -o kubelet
+curl https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kube-proxy -o kube-proxy
+curl https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kubectl -o kubectl
 
-tar -xf kubernetes.tgz
-cd kubernetes/server
-tar -xf kubernetes-server-linux-amd64.tar.gz 
-cd kubernetes/server/bin
-
+echo "Compute Checksum"
 API_SERVER=$(shasum -a 256 kube-apiserver | awk '{ print $1 }')
 CONTROLLER=$(shasum -a 256 kube-controller-manager | awk '{ print $1 }')
 SCHEDULER=$(shasum -a 256 kube-scheduler | awk '{ print $1 }')
@@ -36,6 +32,7 @@ KUBECTL=$(shasum -a 256 kubectl | awk '{ print $1 }')
 cd $CODE_DIR
 rm -rf /tmp/$$.dl
 
+echo "Add checksums to group_vars/all.yml"
 echo "    ${K8S_VERSION}:" >> group_vars/all.yml
 echo "      kube-apiserver: \"sha256:$API_SERVER\"" >> group_vars/all.yml
 echo "      kube-controller-manager: \"sha256:$CONTROLLER\"" >> group_vars/all.yml
